b884a72713482aad1c8eb97953d39b0841a3da93
danielreboucas <daniel.queiroz@rodobank.com.br>
2025-02-10 16:19:22 -0300

    feat: initial products crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule],
+  imports: [UserModule, PrismaModule, AuthModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ test/product/product.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ProductController } from '../../src/product/product.controller';
+import { ProductService } from '../../src/product/product.service';
+
+describe('ProductController', () => {
+  let controller: ProductController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [ProductController],
+      providers: [ProductService],
+    }).compile();
+
+    controller = module.get<ProductController>(ProductController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
