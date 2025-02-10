7418555be00b49173dab4294abc0071f1f7e3354
danielreboucas <daniel.queiroz@rodobank.com.br>
2025-02-10 16:17:39 -0300

    fix: fixed http exceptions filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
-import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { CatchEverythingFilter } from './global-filters/all-exception.filter';
+import { HttpExceptionFilter } from './global-filters/http-exception.filter';
 import { PrismaExceptionFilter } from './global-filters/prisma-exception.filter';
 
 async function bootstrap() {
@@ -16,8 +16,8 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  const httpAdapterHost = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new CatchEverythingFilter(httpAdapterHost));
+  // const httpAdapterHost = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
